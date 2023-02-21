038c2b8cd48b1b5edaa8bae25134cf798b9f99ca
Hazel Windle <hwindle83@googlemail.com>
2023-02-21 18:39:09 +0000

    Setup Express boilerplate code

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => {
+  console.log('Express, Mongoose API listening port: ', PORT);
+});
+
+// localhost:3000/productsapi endpoint - 
+app.get('/productsapi', getAllMakeup);
